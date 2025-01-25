c9c4a133d15818b0286e21b2dc1eea32db42f4b3
Kartik <kartiksharma242001@gmail.com>
2025-01-25 18:39:16 +0530

    added category itemcomposable

--- RecipeApp/app/src/main/java/com/example/recipeapp/RecipeScreen.kt
+++ RecipeApp/app/src/main/java/com/example/recipeapp/RecipeScreen.kt
@@ -1,7 +1,11 @@
 package com.example.recipeapp
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -9,6 +13,11 @@ import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import java.nio.file.WatchEvent
 
 @Composable
 fun RecipeScreen(modifier: Modifier = Modifier){
@@ -27,5 +36,28 @@ fun RecipeScreen(modifier: Modifier = Modifier){
             }
         }
 
+    }
+}
+
+
+@Composable
+fun CategoryScreen(categories:List<Category>){
+    LazyVerticalGrid(GridCells.Fixed(2), modifier = Modifier.fillMaxSize()) { }
+}
+
+// How each item look
+
+@Composable
+fun CategoryItem(category: Category){
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = category.strCategory,
+            color = Color.Black,
+            style = TextStyle(fontWeight = FontWeight.Bold),
+            modifier = Modifier.padding(top = 4.dp)
+        )
     }
 }
