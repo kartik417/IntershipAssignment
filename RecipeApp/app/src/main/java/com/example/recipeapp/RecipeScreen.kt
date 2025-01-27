b16e83eebe6f996e8b61a6ebd1198376691056ba
Kartik <kartiksharma242001@gmail.com>
2025-01-27 10:20:10 +0530

    completed

--- RecipeApp/app/src/main/java/com/example/recipeapp/RecipeScreen.kt
+++ RecipeApp/app/src/main/java/com/example/recipeapp/RecipeScreen.kt
@@ -1,11 +1,14 @@
 package com.example.recipeapp
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,6 +20,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImagePainter.State.Empty.painter
+import coil.compose.rememberAsyncImagePainter
 import java.nio.file.WatchEvent
 
 @Composable
@@ -32,7 +37,7 @@ fun RecipeScreen(modifier: Modifier = Modifier){
                 Text("ERROR OCCURRED")
             }
             else->{
-                
+                CategoryScreen(categories = viewState.recipes)
             }
         }
 
@@ -42,7 +47,12 @@ fun RecipeScreen(modifier: Modifier = Modifier){
 
 @Composable
 fun CategoryScreen(categories:List<Category>){
-    LazyVerticalGrid(GridCells.Fixed(2), modifier = Modifier.fillMaxSize()) { }
+    LazyVerticalGrid(GridCells.Fixed(2), modifier = Modifier.fillMaxSize()) {
+       items(categories){
+           category ->
+           CategoryItem(category = category)
+       }
+    }
 }
 
 // How each item look
@@ -53,6 +63,11 @@ fun CategoryItem(category: Category){
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Image(
+            painter = rememberAsyncImagePainter(category.strCategoryThumb),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize().aspectRatio(1f)
+        )
         Text(
             text = category.strCategory,
             color = Color.Black,
