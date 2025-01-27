f104e8b2bfb430456a6bdce90fb310f11c948817
Kartik <kartiksharma242001@gmail.com>
2025-01-27 10:57:03 +0530

    added first screen

---
+++ NavigationSample/app/src/main/java/com/example/navigationsample/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.navigationsample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.navigationsample.ui.theme.NavigationSampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NavigationSampleTheme {
+                Surface (
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    FirstScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun FirstScreen(){
+    val name = remember { mutableStateOf("") }
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text("This is First Screen",
+            fontWeight = FontWeight.Bold,
+            fontSize = 16.sp
+        )
+        OutlinedTextField(value = name.value, onValueChange = {
+            name.value = it
+        })
+        Button(onClick = {
+
+        }) {
+            Text("Click here to go to Second Screen")
+        }
+    }
+}
